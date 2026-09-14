e715c9180180795b9459e379455b68a6f23d987e
Abdullah Al Naeem <abdullahalnaeem37@gmail.com>
2026-09-14 17:11:53 +0600

    nav added

--- src/Components/TechStackCard.tsx
+++ src/Components/TechStackCard.tsx
@@ -1,73 +1,73 @@
-import { useState, type Dispatch, type SetStateAction } from 'react';
-import { FaStar } from "react-icons/fa";
-import type { TechType } from '../Types/TechType';
-import { Bounce, toast } from 'react-toastify';
+// import { useState, type Dispatch, type SetStateAction } from 'react';
+// import { FaStar } from "react-icons/fa";
+// import type { TechType } from '../Types/TechType';
+// import { Bounce, toast } from 'react-toastify';
 
-interface TechStackCardProps {
-    tech: TechType;
-    selectedStack: TechType[];
-    setSelectedStack: Dispatch<SetStateAction<TechType[]>>;
-}
+// interface TechStackCardProps {
+//     tech: TechType;
+//     selectedStack: TechType[];
+//     setSelectedStack: Dispatch<SetStateAction<TechType[]>>;
+// }
 
-const TechStackCard = ({ tech, selectedStack, setSelectedStack }: TechStackCardProps) => {
-      const [isSelected, setIsSelected] = useState<boolean>(false);
+// const TechStackCard = ({ tech, selectedStack, setSelectedStack }: TechStackCardProps) => {
+//       const [isSelected, setIsSelected] = useState<boolean>(false);
 
-      const handleSelectTech = () => {
-            if (isSelected) return;
+//       const handleSelectTech = () => {
+//             if (isSelected) return;
 
-            setIsSelected(true);
-            setSelectedStack((prev) => [...prev, tech]);
-            toast.success(`${tech.name} Added successfully!`, {
-                  position: "bottom-right",
-                  autoClose: 5000,
-                  hideProgressBar: false,
-                  closeOnClick: false,
-                  pauseOnHover: true,
-                  draggable: true,
-                  progress: undefined,
-                  theme: "light",
-                  transition: Bounce,
-            });
-      };
+//             setIsSelected(true);
+//             setSelectedStack((prev) => [...prev, tech]);
+//             toast.success(`${tech.name} Added successfully!`, {
+//                   position: "bottom-right",
+//                   autoClose: 5000,
+//                   hideProgressBar: false,
+//                   closeOnClick: false,
+//                   pauseOnHover: true,
+//                   draggable: true,
+//                   progress: undefined,
+//                   theme: "light",
+//                   transition: Bounce,
+//             });
+//       };
      
-    return (
+//     return (
      
 
-        <div>
-            <div className="card bg-base-100 w-96 shadow-sm border-1 border-gray-200">
-                <figure className='flex justify-between items-center px-5'>
-                    <img className='w-12 h-24'
-                        src={tech.icon}
-                        alt="" />
-                    <span className='bg-pink-200 text-pink-600 py-1 px-3 rounded-[16px]'>{tech.badge}</span>
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title">{tech.name}</h2>
-                    <p>{tech.description}</p>
-                    <div className='flex justify-between items-center'>
-                        <span className="badge text-[#475569] bg-gray-200">{tech.category}</span>
-                        <span className=" text-[#475569]" >{tech.difficulty}</span>
+//         <div>
+//             <div className="card bg-base-100 w-96 shadow-sm border-1 border-gray-200">
+//                 <figure className='flex justify-between items-center px-5'>
+//                     <img className='w-12 h-24'
+//                         src={tech.icon}
+//                         alt="" />
+//                     <span className='bg-pink-200 text-pink-600 py-1 px-3 rounded-[16px]'>{tech.badge}</span>
+//                 </figure>
+//                 <div className="card-body">
+//                     <h2 className="card-title">{tech.name}</h2>
+//                     <p>{tech.description}</p>
+//                     <div className='flex justify-between items-center'>
+//                         <span className="badge text-[#475569] bg-gray-200">{tech.category}</span>
+//                         <span className=" text-[#475569]" >{tech.difficulty}</span>
 
 
-                        <span className="badge"> <span className='text-yellow-300'><FaStar />
-</span>{tech.rating}</span>
-                    </div>
+//                         <span className="badge"> <span className='text-yellow-300'><FaStar />
+// </span>{tech.rating}</span>
+//                     </div>
                         
-                        <button
-                            onClick={handleSelectTech}
-                            className="btn rounded-[12px] text-white bg-linear-to-r from-[#cb32d9] to-[#8438E4] card-actions justify-centre items-center text-center disabled:opacity-70 disabled:cursor-not-allowed"
-                        >
-                            {isSelected ? "Successfully Added" : "Add to Stack"}
+//                         <button
+//                             onClick={handleSelectTech}
+//                             className="btn rounded-[12px] text-white bg-linear-to-r from-[#cb32d9] to-[#8438E4] card-actions justify-centre items-center text-center disabled:opacity-70 disabled:cursor-not-allowed"
+//                         >
+//                             {isSelected ? "Successfully Added" : "Add to Stack"}
                             
                                
 
-                        </button>
+//                         </button>
                   
                     
-                </div>
-            </div>
-        </div>
-      );
-};
+//                 </div>
+//             </div>
+//         </div>
+//       );
+// };
 
-export default TechStackCard;
+// export default TechStackCard;
--- src/Components/Technologies.tsx
+++ src/Components/Technologies.tsx
@@ -1,20 +1,20 @@
-import React, { use } from 'react';
-import { useState } from 'react';
-import AvailableTechStack from './AvailableTechStack';
-import type { TechType } from '../Types/TechType';
+// import React, { use } from 'react';
+// import { useState } from 'react';
+// import AvailableTechStack from './AvailableTechStack';
+// import type { TechType } from '../Types/TechType';
 
-const Technologies = ({ techDataPromise }: { techDataPromise: Promise<TechType[]> }) => {
-      const technologies = use(techDataPromise);
-      const [selectedStack, setSelectedStack] = useState<TechType[]>([]);
-      return (
-            <div className='container mx-auto'>
-            <div>
-                <h2 className='mt-4 mb-1.5 font-extrabold text-4xl'>Explore the <span className='text-transparent bg-clip-text bg-linear-to-r  from-[#E22B66] to-[#8438E4]'>Technologies</span></h2>
-                <p className='mb-6 text-[16px] text-[#64748B]'>Pick one technology per category to build your ideal stack.</p>
-            </div>
-               <AvailableTechStack selectedStack={selectedStack} setSelectedStack={setSelectedStack} technologies={technologies}></AvailableTechStack>
-        </div>
-      );
-};
+// const Technologies = ({ techDataPromise }: { techDataPromise: Promise<TechType[]> }) => {
+//       const technologies = use(techDataPromise);
+//       const [selectedStack, setSelectedStack] = useState<TechType[]>([]);
+//       return (
+//             <div className='container mx-auto'>
+//             <div>
+//                 <h2 className='mt-4 mb-1.5 font-extrabold text-4xl'>Explore the <span className='text-transparent bg-clip-text bg-linear-to-r  from-[#E22B66] to-[#8438E4]'>Technologies</span></h2>
+//                 <p className='mb-6 text-[16px] text-[#64748B]'>Pick one technology per category to build your ideal stack.</p>
+//             </div>
+//                <AvailableTechStack selectedStack={selectedStack} setSelectedStack={setSelectedStack} technologies={technologies}></AvailableTechStack>
+//         </div>
+//       );
+// };
 
-export default Technologies;
+// export default Technologies;
