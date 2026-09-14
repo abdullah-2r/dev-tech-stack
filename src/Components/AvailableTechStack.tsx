b548d0e58321efd01275d66314d981e9445aef88
Abdullah Al Naeem <abdullahalnaeem37@gmail.com>
2026-09-14 17:14:03 +0600

    available tech stack added

--- src/Components/AvailableTechStack.tsx
+++ src/Components/AvailableTechStack.tsx
@@ -1,34 +1,34 @@
-// import type { Dispatch, SetStateAction } from 'react';
-// import type { TechType } from '../Types/TechType';
-// import TechStackCard from './TechStackCard';
-// import SelectedTechStack from './SelectedTechStack';
+import type { Dispatch, SetStateAction } from 'react';
+import type { TechType } from '../Types/TechType';
+import TechStackCard from './TechStackCard';
+import SelectedTechStack from './SelectedTechStack';
 
-// interface AvailableTechStackProps {
-//       technologies: TechType[];
-//       selectedStack: TechType[];
-//       setSelectedStack: Dispatch<SetStateAction<TechType[]>>;
-// }
+interface AvailableTechStackProps {
+      technologies: TechType[];
+      selectedStack: TechType[];
+      setSelectedStack: Dispatch<SetStateAction<TechType[]>>;
+}
 
-// const AvailableTechStack = ({ technologies, selectedStack, setSelectedStack }: AvailableTechStackProps) => {
-//       return (
-//              <div className="flex flex-col-reverse md:flex-row gap-6 items-start">
-//         <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4 '>
-//             {technologies.map((tech: TechType)=> {
-//                 return (
-//             <TechStackCard 
-//             key={tech.id} 
-//             tech={tech} 
-//             selectedStack={selectedStack} 
-//             setSelectedStack={setSelectedStack}
-//             />
-//         );
-//     })}
-// </div>
-//     <div className="w-full md:w-80 shrink-0 bg-gray-50 p-4 rounded-xl shadow-sm border border-gray-100">
-//                             <SelectedTechStack selectedStack={selectedStack} setSelectedStack={setSelectedStack} />
-//                     </div>
-// </div>
-//       );
-// };
+const AvailableTechStack = ({ technologies, selectedStack, setSelectedStack }: AvailableTechStackProps) => {
+      return (
+             <div className="flex flex-col-reverse md:flex-row gap-6 items-start">
+        <div className='grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-3 gap-4 '>
+            {technologies.map((tech: TechType)=> {
+                return (
+            <TechStackCard 
+            key={tech.id} 
+            tech={tech} 
+            selectedStack={selectedStack} 
+            setSelectedStack={setSelectedStack}
+            />
+        );
+    })}
+</div>
+    <div className="w-full md:w-80 shrink-0 bg-gray-50 p-4 rounded-xl shadow-sm border border-gray-100">
+                            <SelectedTechStack selectedStack={selectedStack} setSelectedStack={setSelectedStack} />
+                    </div>
+</div>
+      );
+};
 
-// export default AvailableTechStack;
+export default AvailableTechStack;
